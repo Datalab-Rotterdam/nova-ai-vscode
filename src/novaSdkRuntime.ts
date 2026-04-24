2d5aec065cad37a402316e1e97ebe6c602b0a711
A.P.A.Slaa <a.p.a.slaa@projectsource.nl>
2026-04-24 21:30:08 +0200

    fix: bundle nova sdk into extension

--- src/novaSdkRuntime.ts
+++
@@ -1,8 +0,0 @@
-type NovaSdkModule = typeof import('@datalabrotterdam/nova-sdk');
-
-const dynamicImport = new Function('specifier', 'return import(specifier)') as (specifier: string) => Promise<unknown>;
-
-export async function loadNovaSdk(): Promise<NovaSdkModule> {
-  return dynamicImport('@datalabrotterdam/nova-sdk') as Promise<NovaSdkModule>;
-}
-
