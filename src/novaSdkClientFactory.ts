c6bd73c9d57161a8d9159bdac1eda4469b941b49
A.P.A.Slaa <a.p.a.slaa@projectsource.nl>
2026-04-24 21:30:08 +0200

    fix: bundle nova sdk into extension

--- src/novaSdkClientFactory.ts
+++ src/novaSdkClientFactory.ts
@@ -1,11 +1,8 @@
-import type { NovaAI } from '@datalabrotterdam/nova-sdk';
+import { NovaAI } from '@datalabrotterdam/nova-sdk';
 import { getBaseUrl } from './config';
-import { loadNovaSdk } from './novaSdkRuntime';
 
 export class NovaSdkClientFactory {
   public async create(apiKey: string): Promise<NovaAI> {
-    const { NovaAI } = await loadNovaSdk();
-
     return new NovaAI({
       apiKey,
       baseUrl: getBaseUrl()
